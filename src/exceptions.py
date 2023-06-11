12489fb9bce19b97c3ba03b6f3997bffec81694b
Erkan <cobanerkan@yahoo.com>
2023-06-11 00:40:32 -0300

    add exceptions

---
+++ src/exceptions.py
@@ -0,0 +1,13 @@
+from typing import Any
+from utils import print_with_time
+
+from constants import USER_DEFINED_STREAMS_FILENAME
+
+
+class UserDefinedStreamsLoadException(Exception):
+    def __init__(self, *args: object) -> None:
+        print_with_time(f"Error reading {USER_DEFINED_STREAMS_FILENAME}")
+        print_with_time("Probably due to all drops are claimed")
+        print_with_time("If file is empty and you have more drops to claim, add streams to file")
+        print_with_time("Check https://github.com/cocorocho/RustTwitchDrops for more info")
+        exit(1)
